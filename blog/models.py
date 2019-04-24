b58e5dd299ce50c0d64da1b32870763f0cab0112
cebasypia <cebasypia.tskm8@gmail.com>
2019-04-24 12:32:16 +0900

    switch null with blank

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    document = models.FileField(upload_to='documents', null=True, blank=True)
+    document = models.FileField(upload_to='documents', blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
